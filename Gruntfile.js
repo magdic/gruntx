85dafa944979226dc7ab8416d14c4c579004602f
Magdiel <luisc@luisc-thi-m.cmass.criticalmass.com>
2014-01-10 10:11:09 -0600

    Commit with all the grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+     connect: {
+    server: {
+      options: {
+        port: 9001
+      }
+    }
+  },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    },
+    express: {
+    options: {
+      // Override defaults here
+    },
+    dev: {
+      options: {
+        script: 'server.js',
+        app: 'Google Chrome'
+      }
+    },
+    prod: {
+      options: {
+        script: 'path/to/prod/server.js',
+        node_env: 'production'
+      }
+    },
+    test: {
+      options: {
+        script: 'path/to/test/server.js'
+      }
+    }
+  },
+    open : {
+      dev : {
+        path: 'http://localhost:1337/',
+        app: 'Google Chrome'
+    }/*
+      build : {
+        path : 'http://google.com/',
+        app: 'Firefox'
+    },
+      file : {
+        path : '/etc/hosts'
+    }*/
+  }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'open']);
+
+  grunt.registerTask('server', [ 'express:dev', 'open', 'watch' ]);
+
+};
